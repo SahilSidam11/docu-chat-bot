6f5af8306294144ca7274b273e1c9751ea2c37b0
Sahil Sidam <sahilsidam03@gmail.com>
2024-07-15 22:37:29 +0530

    implement dashboard, display and delete files functionalities and API procedures

---
+++ src/app/dashboard/[fileid]/page.tsx
@@ -0,0 +1,15 @@
+interface PageProps {
+  params: {
+    fileid: string;
+  };
+}
+
+const Page = ({ params }: PageProps) => {
+  // retrieve the file id
+
+  // make database call
+
+  return <div>Page</div>;
+};
+
+export default Page;
